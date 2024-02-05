4fea165fcec4c3a3258915f336d4f3ebcf1c9b91
Dan Mindru <mindrudan@gmail.com>
2024-02-05 16:47:11 +0100

    Build website

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import { ResponsiveIframeViewer, ViewportSize } from "../lib/main";
+import { GithubIcon, ExternalLinkIcon } from "lucide-react";
 
 function App() {
   return (
     <div className="flex flex-col items-center p-4 gap-12">
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center">
         <h1 className="m-0">React Responsive Iframe Viewer</h1>
         <p>Display iframes responsively in your React app, in style.</p>
+
+        <a
+          href="https://github.com/danmindru/react-responsive-iframe-viewer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2"
+        >
+          <GithubIcon /> Get it on GitHub <ExternalLinkIcon />
+        </a>
       </div>
 
       <ResponsiveIframeViewer
@@ -20,14 +30,14 @@ function App() {
       <ResponsiveIframeViewer
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         title="Rick Astley - Never Gonna Give You Up"
-        size={ViewportSize.mobile}
+        size={ViewportSize.desktop}
         allowResizingY
       />
 
       <ResponsiveIframeViewer
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         title="Rick Astley - Never Gonna Give You Up"
-        size={ViewportSize.mobile}
+        size={ViewportSize.tablet}
         allowResizingX
       />
     </div>
